ab0c279b47d106eade36798106aefdd22f98769b
Kingjibes <160682090+Kingjibes@users.noreply.github.com>
2025-05-21 23:23:25 +0000

    Update AudioContext.jsx

--- src/contexts/AudioContext.jsx
+++ src/contexts/AudioContext.jsx
@@ -13,7 +13,7 @@ const SONGS = [
   { id: 8, url: '/assets/background_music8.mp3', name: 'Cyberpunk City' },
   { id: 9, url: '/assets/background_music9.mp3', name: 'Piano Reflections' },
   { id: 10, url: '/assets/background_music10.mp3', name: 'Ocean Waves' },
-  { id: 10, url: '/assets/background_music11.mp3', name: 'Ocean Waves' }
+  { id: 11, url: '/assets/background_music11.mp3', name: 'Ocean Waves' }
 ];
 
 export const AudioProvider = ({ children }) => {
